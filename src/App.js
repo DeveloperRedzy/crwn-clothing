07565c955856201b8f60542a964ee0ec350d23c9
Rijad Kuloglija <redzy@Rijads-MBP.Dlink>
2021-01-31 16:26:48 +0300

    homepage component and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import Homepage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component.jsx";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <HomePage />
     </div>
   );
 }
