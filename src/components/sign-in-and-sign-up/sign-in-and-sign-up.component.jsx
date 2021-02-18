5492f42136e308ee4604a895a227df83b0fb3ba5
Rijad Kuloglija <redzy@Rijads-MBP.Dlink>
2021-02-18 18:23:25 +0300

    SignIn,CustomButton,FormInput...

---
+++ src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './sign-in-and-sing-up.style.scss';
+import SignIn from '../../components/sign-in/sing-in.component';
+const SignInAndSignUpPage = () => {
+  return (
+    <div className='sign-in-and-sign-up'>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;
